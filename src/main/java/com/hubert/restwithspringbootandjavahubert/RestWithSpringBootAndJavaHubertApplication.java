006a1b7ec0b0e56035e6ea2d4078d96acda6a6a2
HubertFer <humbertofernandezsosa@gmail.com>
2023-07-24 17:22:16 -0300

    Upload project to main

---
+++ src/main/java/com/hubert/restwithspringbootandjavahubert/RestWithSpringBootAndJavaHubertApplication.java
@@ -0,0 +1,13 @@
+package com.hubert.restwithspringbootandjavahubert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaHubertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaHubertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hubert/restwithspringbootandjavahubert/RestWithSpringBootAndJavaHubertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubert.restwithspringbootandjavahubert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaHubertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
